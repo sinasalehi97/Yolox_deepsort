8f2c8d54d6d41abbebbcb6fab0737f9f20d92e73
sinasalehi97 <103408826+sinasalehi97@users.noreply.github.com>
2022-06-26 15:04:29 -0700

    Update Track_count.py

--- Track_count.py
+++ Track_count.py
@@ -213,25 +213,19 @@ if __name__=='__main__':
         ret_val, frame = cap.read() # read frame from video
         t1 = time_synchronized()
 
-        for i in range(0, detections.shape[2]):
-            # filter out weak detections by ensuring the predicted
-		    # probability is greater than a minimum threshold
-            if detections[0, 0, i, 2] > args["confidence"]:
-			   # compute the (x, y)-coordinates of the bounding box for
-			   # the object, then update the bounding box rectangles list
-			   box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-			   rects.append(box.astype("int"))
+  
 
         objects = ct.update(rects)
 
 	# loop over the tracked objects
-	    for (objectID, centroid) in objects.items():
-		     # draw both the ID of the object and the centroid of the
-		     # object on the output frame
-		     text = "ID {}".format(objectID)
-		     cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-			 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-		     cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+	for (objectID, centroid) in objects.items():
+		
+	        # draw both the ID of the object and the centroid of the
+		# object on the output frame
+                text = "ID {}".format(objectID)
+      	        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+	        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
             
 
         if ret_val:
